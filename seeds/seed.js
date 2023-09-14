355ccced3382c781ce903904d6609fadec7e7d80
BD3849 <bsdinnan@icloud.com>
2023-09-14 04:34:04 -0400

    fixed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,14 +1,28 @@
+const seedUsers = require('./userData')
 const seedEmployees = require('./employeeData.json');
 const seedRoles = require('./roleData.json');
 const seedManagers = require('./managerData.json');
 const seedDepartments = require('./departmentData.json');
 const seedTimepunch = require('./timepunchData.json');
 
+const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection');
 const { User, Employee, Role, Manager, Department, Timepunch } = require('../models');
 
+
+
 const seedDatabase = async () => {
-    await sequelize.sync();
+    await sequelize.sync({ force: true });
+
+    const hashedUsers = await Promise.all(seedUsers.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return {
+            username: user.username,
+            password: hashedPassword,
+        };
+    }));
+
+    await User.bulkCreate(hashedUsers);
 
     await Department.bulkCreate(seedDepartments, {
         individualHooks: true,
