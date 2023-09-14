a52c802c1c25a2d03867e2bdd0cddbed209ff289
Gage Hamel <Gagehamel21@gmail.com>
2023-09-14 19:17:38 -0400

    clockbtn code

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 app.get('/', async (req, res) => {
-    res.render('login');
+    res.render('profile');
 });
 
 sequelize.sync({ force: false }).then(() => {
